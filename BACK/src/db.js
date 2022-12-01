738a0be3a5e953950b8be4e39f6709a27b5ffd12
Jesús Datica <jesusado.89@gmail.com>
2022-12-01 15:56:34 -0400

    routes location

--- BACK/src/db.js
+++ BACK/src/db.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
-const mongoose = require ('mongoose');
-const {PASSWORD, DB_DEPLOY} = process.env;
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { PASSWORD, DB_DEPLOY } = process.env;
 
-// const uri = `mongodb+srv://noesneda:${PASSWORD}@proyecto.hbyeplt.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://noesneda:${PASSWORD}@proyecto.hbyeplt.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(DB_DEPLOY, {
+mongoose.connect(
+  uri,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, console.log('DB is conected'))
+    useUnifiedTopology: true,
+  },
+  console.log("DB is conected")
+);
