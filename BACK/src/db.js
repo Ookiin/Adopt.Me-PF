94ef29a3be5f39150d59696691273b3a64bbb18c
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2022-12-01 18:34:59 -0300

    Condicional registro para dar en adopcion

--- BACK/src/db.js
+++ BACK/src/db.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const mongoose = require ('mongoose');
 const {PASSWORD, DB_DEPLOY} = process.env;
 
-// const uri = `mongodb+srv://noesneda:${PASSWORD}@proyecto.hbyeplt.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://noesneda:${PASSWORD}@proyecto.hbyeplt.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(DB_DEPLOY, {
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, console.log('DB is conected'))
