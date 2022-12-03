999601cd77981bd78d7610caa2fde1d10a6179c5
Andres <andrescantlon@gmail.com>
2022-12-03 13:36:43 -0300

    stripes no anda

--- front/src/Componentes/Stripes/Stripe.js
+++ front/src/Componentes/Stripes/Stripe.js
@@ -1,90 +1,75 @@
-import React, { useState } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import {Elements, CardElement, useStripe, useElements} from '@stripe/react-stripe-js'
-import axios from 'axios'
+// import React, { useState } from 'react';
+// import { loadStripe } from '@stripe/stripe-js';
+// import {Elements, CardElement, useStripe, useElements} from '@stripe/react-stripe-js'
+// import axios from 'axios'
 
-import 'bootswatch/dist/litera/bootstrap.min.css'
-import './App.css';
+// // import 'bootswatch/dist/litera/bootstrap.min.css'
 
-const stripePromise = loadStripe("pk_test_51M9wCWDFa6jLCwa92xpEczTJ20PPSzL6XTQmi8OZan1aNkHFGAv9A0AX7OJIw8wJf1Ru59PdJrRpGnnBvopiFK5T000lEuF7rB")
 
-const CheckoutForm = () => {
-    
-     const [loading, setLoading] = useState(false)
-  /* const [email, setEmail] = useState("");
-  const [name, setName] = useState(""); */
+// const stripePromise = loadStripe("pk_test_51M9wCWDFa6jLCwa92xpEczTJ20PPSzL6XTQmi8OZan1aNkHFGAv9A0AX7OJIw8wJf1Ru59PdJrRpGnnBvopiFK5T000lEuF7rB")
 
-  const stripe = useStripe();
-  const elements = useElements();
+// const CheckoutForm = () => {
 
-  const handleSubmit = async (e) =>{
-    e.preventDefault();
+//      const [loading, setLoading] = useState(false)
+//   /* const [email, setEmail] = useState("");
+//   const [name, setName] = useState(""); */
 
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-      type:'card',
-      card:elements.getElement(CardElement)
+//   const stripe = useStripe();
+//   const elements = useElements();
 
-    });
-    setLoading(true)
+//   const handleSubmit = async (e) =>{
+//     e.preventDefault();
 
-    if(!error){
-      const { id } = paymentMethod;
+//     const {error, paymentMethod} = await stripe.createPaymentMethod({
+//       type:'card',
+//       card:elements.getElement(CardElement)
 
-      try {
-        const {data} = await axios.post('http://localhost:3001/api/checkout', {
-        id,
-        amount: (20 * 100),
-        /* email: "email",
-        name: "name" */
-      })
+//     });
+//     setLoading(true)
 
-      console.log(data);
+//     if(!error){
+//       const { id } = paymentMethod;
 
-      elements.getElement(CardElement).clear();
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    setLoading(false)
-  }
+//       try {
+//         const {data} = await axios.post('http://localhost:3001/api/checkout', {
+//         id,
+//         amount: (20 * 100),
+//         /* email: "email",
+//         name: "name" */
+//       })
 
-  return <form onSubmit={handleSubmit} className="card card-body">
-     <img 
-     src={`https://i.pinimg.com/736x/35/23/43/352343ff27c32d65f42f976abb320efb.jpg`} 
-     alt="Adopt.me" 
-     className="img-fluid"/>
+//       console.log(data);
 
-     <h3 className="text-center my-2">Tu Donativon Salva</h3>
-    
-    <div className="form-group">
-      <CardElement className="form-control" />
-    </div>
-    
-    <button className="btn btn-success" disabled={!stripe}>
-      {loading ? (
-        <div className="spinner-border text-light" role="status">
-    <span className='sr-only'></span>
-        </div>
-      ) : (
-        "Donativo"
-      )}
-    </button>
-  </form>
-}
+//       elements.getElement(CardElement).clear();
+//       } catch (error) {
+//         console.log(error)
+//       }
+//     }
+//     setLoading(false)
+//   }
 
-function App() {
-  return (
-    <Elements stripe={stripePromise}>
-      <div className="container p-4">
-        <div className="row">
-          <div className="col-md-4 offset-md-4">
-      <CheckoutForm/>
-          </div>
-        </div>
-      </div>
-    </Elements>
-    
-  );
-}
+//   return <form onSubmit={handleSubmit} className="card card-body">
+//      {/* <img 
+//      src={`https://i.pinimg.com/736x/35/23/43/352343ff27c32d65f42f976abb320efb.jpg`} 
+//      alt="Adopt.me" 
+//      className="img-fluid"/> */}
 
-export default App;
+//      <h3 className="text-center my-2">Tu Donativon Salva</h3>
+
+//     <div className="form-group">
+//       <CardElement className="form-control" />
+//     </div>
+
+//     <button className="btn btn-success" disabled={!stripe}>
+//       {loading ? (
+//         <div className="spinner-border text-light" role="status">
+//     <span className='sr-only'></span>
+//         </div>
+//       ) : (
+//         "Donativo"
+//       )}
+//     </button>
+//   </form>
+// }
+
+// export default CheckoutForm
