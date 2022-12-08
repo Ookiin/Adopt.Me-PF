7fcfa4dba8b94e76a56ce6af3d2fe3c28b0a8653
Alejandra Aragon <alejita.0418@gmail.com>
2022-12-07 22:24:07 -0300

    Graficas de mascotas harckodeadas

---
+++ front/src/Componentes/PAdmChartsMascotas/Perdidos.js
@@ -0,0 +1,52 @@
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import stl from "../PAdmChartsMascotas/Perdidos.module.css"
+
+const data = [
+  {
+    name: 'Perros',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Gatos',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  
+];
+
+export default function Perdidos (){
+  
+
+  
+return (
+  <div className={stl.grafica}>
+      <div className={stl.title}>MASCOTAS PERDIDAS</div>
+      <ResponsiveContainer width="70%" aspect={3}>
+        <LineChart
+          width={70}
+          height={400}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+      </div>
+    );
+  
+}
--- front/src/Componentes/PanelAItems/mascotas.js
+++ front/src/Componentes/PanelAItems/mascotas.js
@@ -1,14 +1,35 @@
 import React from "react";
-import EnAdopcionChart from "../PanelAdminCharts/EnAdopcionChart1"
+import EstadoMascotas from "../PAdmChartsMascotas/EstadoMascotas";
+import EnAdopcion from "../PAdmChartsMascotas/EnAdopcion";
+import Adoptados from "../PAdmChartsMascotas/Adoptados";
+import Perdidos from "../PAdmChartsMascotas/Perdidos";
+import Buscados from "../PAdmChartsMascotas/Buscados";
 import NavBarPAdmin from "../PanelAdministrador/NavBarPAdmin" 
 
 const Mascotas = () => {
   return(
     <div>
-       <NavBarPAdmin /> 
-       <EnAdopcionChart />
-   <div>
-  </div>
+      <div>
+        <h2>INFORME GENERAL DE NUESTRAS MASCOTAS</h2>
+      </div>
+      <div>
+        <NavBarPAdmin /> 
+      </div>
+      <div>
+       <EstadoMascotas />
+      </div>
+      <div>
+       <EnAdopcion />
+      </div>
+      <div>
+       <Adoptados /> 
+      </div>
+      <div>
+       <Perdidos />
+      </div>
+      <div>
+       <Buscados/>
+      </div>   
     </div>
   )
 }
