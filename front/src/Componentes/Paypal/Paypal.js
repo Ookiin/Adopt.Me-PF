d94185f4091dbaeb5acf0a97c58d88a3510366da
Jesús Datica <jesusado.89@gmail.com>
2022-12-11 17:57:24 -0400

    pagos paypal

--- front/src/Componentes/Paypal/Paypal.js
+++ front/src/Componentes/Paypal/Paypal.js
@@ -1,42 +1,52 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import stl from "../Paypal/Paypal.module.css";
-import Toast from 'light-toast';
+import Toast from "light-toast";
 
 export default function Paypal() {
+  const [price, setPrice] = useState(0);
 
-    const paypal = useRef();
+  const paypal = useRef();
 
-    useEffect(() => {
-        window.paypal.Buttons({
-            createOrder: (data, actions, err) => {
-                return actions.order.create({
-                    intent: "CAPTURE",
-                    purchase_units: [
-                        {
-                            description: "Donacion",
-                            amount: {
-                                currency_code: "USD",
-                                value: 1
-                            }
-                        }
-                    ]
-                })
-            },
-            onApprove: async (data, actions) => {
-                const order = await actions.order.capture()
-                console.log(order)
-               Toast.success('SU DONACIÓN A SIDO REALIZADA CON EXITO', 1500, () => {});
-                window.location.replace('/homepage')
-            },
-            onError: (err) => {
-                console.log(err)
-            }
-        }).render(paypal.current)
-    }, [])
+  useEffect(() => {
+    window.paypal
+      .Buttons({
+        createOrder: (data, actions, err) => {
+          return actions.order.create({
+            intent: "CAPTURE",
+            purchase_units: [
+              {
+                description: "Donacion",
+                amount: {
+                  currency_code: "USD",
+                  value: price,
+                },
+              },
+            ],
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          console.log(order);
+          Toast.success(
+            "SU DONACIÓN A SIDO REALIZADA CON EXITO",
+            1500,
+            () => {}
+          );
+          window.location.replace("/homepage");
+        },
+        onError: (err) => {
+          console.log(err);
+        },
+      })
+      .render(paypal.current);
+  }, []);
 
-    return (
-      
-            <div className={stl.botonpaypal} ref={paypal}></div>
-        
-    )
+  function handleChange(e) {
+    setPrice(e.target.value);
+  }
+
+  return (
+    <div className={stl.botonpaypal} ref={paypal}></div>
+    //<input type="text" onChange={handleChange} value={price}></input>
+  );
 }
